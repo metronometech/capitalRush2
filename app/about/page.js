75febe07a27e0867b020167dc649c5c595604d44
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-28 13:54:40 +0530

    check2

--- app/about/page.js
+++ app/about/page.js
@@ -6,37 +6,14 @@ import About1 from "@/components/About/About1";
 
 const page = () => {
 
-  const [link,updatelink]=useState("/");
-
-  function setAppStoreLink() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    
-    // Check for iOS (iPhone, iPad, iPod)
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        // document.getElementById('appStoreLink').href = 'https://apps.apple.com/us/app/your-app-name/id1234567890'; // Replace with your actual App Store link
-        updatelink("https://apps.apple.com/in/app/capital-rush/id1615705403")
-    } 
-    // Check for Android
-    else if (/android/i.test(userAgent)) {
-        // document.getElementById('playStoreLink').href = 'https://play.google.com/store/apps/details?id=com.yourapp.package'; // Replace with your actual Play Store link
-        updatelink("https://play.google.com/store/apps/details?id=com.metronome.capital")
-    }
-}
-
-useEffect(() => {
-  setAppStoreLink();
-}, [])
-
-  
-
   return (
     <div className="">
       <div className="bg-[#100B26]">
         <div className=" hidden md:block mb-32">
-          <Nav2 link={link} />
+          <Nav2 />
         </div>
         <div className=" block md:hidden pt-4">
-          <Nav2 variant="mobile" link={link} />
+          <Nav2 variant="mobile" />
         </div>
         <About1 />
       </div>
--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,4 +1,5 @@
- import React from "react";
+"use client"
+import React from "react";
 import "../../pagemodule.css";
 import Nav2 from "@/components/common/Nav2";
 import SepBlog from "@/components/Blog/SepBlog";
