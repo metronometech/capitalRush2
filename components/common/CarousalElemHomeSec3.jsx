a3d815625693009ed212f1077202f72e8137029f
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-10-01 17:08:07 +0530

    slider fixed

--- components/common/CarousalElemHomeSec3.jsx
+++ components/common/CarousalElemHomeSec3.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 import "./caurosal3.css";
@@ -7,25 +7,41 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 import Image from "next/image";
 
 const CarousalElemHomeSec3 = () => {
+  const nextButtonRef = useRef(null);
+  const [move,updateMove]=useState(300);
+
+  function decrement(){
+    updateMove(prevMove => (prevMove - 1));
+  }
+  function increment(){
+    updateMove(prevMove => (prevMove + 1));
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (nextButtonRef.current) {
+        nextButtonRef.current.click();
+        updateMove(prevMove => (prevMove + 1));
+      }
+    }, 3000);
+
+    return () => clearInterval(interval); // Clear the interval on component unmount
+  }, []);
+
   return (
     <div className="carousel-container flex-col ">
       <div className="py-10  flex xl:gap-20 md:gap-8  2xl:mx-40 mb:mx-16  items-center ">
         <div className=" carousel-buttons z-30">
-          <button className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-prev">
-            <Image
-              alt="left-arrow"
-              src="/left.png"
-              height={60}
-              width={60}
-            />
+          <button onClick={decrement} className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-prev">
+            <Image alt="left-arrow" src="/left.png" height={60} width={60} />
           </button>
           <button className="block md:hidden carousel-button hzoom1 cursor-pointer carousel-button-prev">
-            <Image
+            {/* <Image
               alt="left-arrow"
               src="/left.png"
               height={40}
               width={40}
-            />
+            /> */}
           </button>
         </div>
         <Swiper
@@ -47,7 +63,7 @@ const CarousalElemHomeSec3 = () => {
           className="mySwiper"
         >
           <div className="">
-            <SwiperSlide  className="group">
+            <SwiperSlide className="group">
               <Image
                 alt="Carosal-home-Images"
                 className=" group-hover:brightness-125"
@@ -106,27 +122,38 @@ const CarousalElemHomeSec3 = () => {
           </div>
         </Swiper>
         <div className="carousel-buttons z-30">
-          <button className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-next">
-            <Image
-              alt="right-arrow"
-              src="/right.png"
-              height={60}
-              width={60}
-            />
+          <button onClick={increment} className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-next">
+            <Image alt="right-arrow" src="/right.png" height={60} width={60} />
           </button>
-          <button className="block md:hidden carousel-button hzoom1 cursor-pointer carousel-button-next">
-            <Image
+          <button
+            ref={nextButtonRef}
+            className="block md:hidden carousel-button hzoom1 cursor-pointer carousel-button-next"
+          >
+            {/* <Image
               alt="right-arrow"
               src="/right.png"
               height={40}
               width={40}
-            />
+            /> */}
           </button>
         </div>
       </div>
       {/* <div className="z-40 text-center ">
-        <div className="swiper-pagination"></div>
+        <div className=" text-white swiper-pagination"></div>
       </div> */}
+      <div className=" -mt-16 mb:-mt-12 relative">
+        <p className=" text-center text-white text-[30px]">
+          {move %3===0 && <span>
+            <span className="text-[45px]">.</span> . .
+          </span>}
+          {move%3===1 && <span>
+            <span className="text-[45px]">. .</span>  .
+          </span>}
+          {move%3===2 && <span>
+            <span className="text-[45px]">. . .</span> 
+          </span>}
+        </p>
+      </div>
     </div>
   );
 };
