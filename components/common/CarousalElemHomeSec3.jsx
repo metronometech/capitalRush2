c953da9dd71edabcbe8ab2c2ff9fb579a1a8be8d
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-10-01 23:30:51 +0530

    done carosal

--- components/common/CarousalElemHomeSec3.jsx
+++ components/common/CarousalElemHomeSec3.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
@@ -8,20 +7,22 @@ import Image from "next/image";
 
 const CarousalElemHomeSec3 = () => {
   const nextButtonRef = useRef(null);
-  const [move,updateMove]=useState(300);
+  const [move, updateMove] = useState(300);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
-  function decrement(){
-    updateMove(prevMove => (prevMove - 1));
+  function decrement() {
+    updateMove((prevMove) => prevMove - 1);
   }
-  function increment(){
-    updateMove(prevMove => (prevMove + 1));
+
+  function increment() {
+    updateMove((prevMove) => prevMove + 1);
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (nextButtonRef.current) {
         nextButtonRef.current.click();
-        updateMove(prevMove => (prevMove + 1));
+        updateMove((prevMove) => prevMove + 1);
       }
     }, 4000);
 
@@ -29,10 +30,13 @@ const CarousalElemHomeSec3 = () => {
   }, []);
 
   return (
-    <div className="carousel-container flex-col ">
-      <div className="py-10  flex xl:gap-20 md:gap-8  2xl:mx-40 mb:mx-16  items-center ">
-        <div className=" carousel-buttons z-30">
-          <button onClick={decrement} className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-prev">
+    <div className="carousel-container flex-col">
+      <div className="py-10 flex xl:gap-20 md:gap-8 2xl:mx-40 md:mx-16 items-center">
+        <div className="carousel-buttons z-30">
+          <button
+            onClick={decrement}
+            className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-prev"
+          >
             <Image alt="left-arrow" src="/left.png" height={60} width={60} />
           </button>
           <button className="block md:hidden carousel-button hzoom1 cursor-pointer carousel-button-prev">
@@ -45,6 +49,9 @@ const CarousalElemHomeSec3 = () => {
           </button>
         </div>
         <Swiper
+          onSlideChange={(swiper) => {
+            setCurrentSlideIndex(swiper.activeIndex);
+          }}
           navigation={{
             prevEl: ".carousel-button-prev",
             nextEl: ".carousel-button-next",
@@ -53,7 +60,6 @@ const CarousalElemHomeSec3 = () => {
             el: ".swiper-pagination",
             clickable: true,
           }}
-          // pagination={true}
           mousewheel={true}
           keyboard={true}
           slidesPerView={1}
@@ -66,7 +72,7 @@ const CarousalElemHomeSec3 = () => {
             <SwiperSlide className="group">
               <Image
                 alt="Carosal-home-Images"
-                className=" group-hover:brightness-125"
+                className="group-hover:brightness-125"
                 src="/mobile.png"
                 fill
               />
@@ -74,7 +80,7 @@ const CarousalElemHomeSec3 = () => {
             <SwiperSlide className="group">
               <Image
                 alt="Carosal-home-Images"
-                className="  group-hover:brightness-125"
+                className="group-hover:brightness-125"
                 src="/mobile1.png"
                 fill
               />
@@ -82,47 +88,18 @@ const CarousalElemHomeSec3 = () => {
             <SwiperSlide className="group">
               <Image
                 alt="Carosal-home-Images"
-                className=" group-hover:brightness-125"
+                className="group-hover:brightness-125"
                 src="/mobile3.png"
                 fill
               />
             </SwiperSlide>
-            {/* <SwiperSlide className="group">
-              <Image
-                alt="Carosal-home-Images"
-                className=" group-hover:brightness-125"
-                src="/mobile.png"
-                fill
-              />
-            </SwiperSlide> */}
-            {/* <SwiperSlide className="group">
-              <Image
-                alt="Carosal-home-Images"
-                className=" group-hover:brightness-125"
-                src="/mobile.png"
-                fill
-              />
-            </SwiperSlide> */}
-            {/* <SwiperSlide className="group">
-              <Image
-                alt="Carosal-home-Images"
-                className=" group-hover:brightness-125"
-                src="/mobile.png"
-                fill
-              />
-            </SwiperSlide> */}
-            {/* <SwiperSlide className="group">
-              <Image
-                alt="Carosal-home-Images"
-                className=" group-hover:brightness-125"
-                src="/mobile.png"
-                fill
-              />
-            </SwiperSlide> */}
           </div>
         </Swiper>
         <div className="carousel-buttons z-30">
-          <button onClick={increment} className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-next">
+          <button
+            onClick={increment}
+            className="hidden md:block carousel-button hzoom1 cursor-pointer carousel-button-next"
+          >
             <Image alt="right-arrow" src="/right.png" height={60} width={60} />
           </button>
           <button
@@ -138,20 +115,23 @@ const CarousalElemHomeSec3 = () => {
           </button>
         </div>
       </div>
-      {/* <div className="z-40 text-center ">
-        <div className=" text-white swiper-pagination"></div>
-      </div> */}
-      <div className=" -mt-16 mb:-mt-12 relative">
-        <p className=" text-center text-white text-[30px]">
-          {move %3===0 && <span>
-            <span className="text-[45px]">.</span> . .
-          </span>}
-          {move%3===1 && <span>
-            <span className="text-[45px]">. .</span>  .
-          </span>}
-          {move%3===2 && <span>
-            <span className="text-[45px]">. . .</span> 
-          </span>}
+      <div className="-mt-16 mb:-mt-12 relative">
+        <p className="text-center text-[40px] text-gray-100">
+          {currentSlideIndex % 3 === 0 && (
+            <span>
+              <span className="text-white">.</span> . .
+            </span>
+          )}
+          {currentSlideIndex % 3 === 1 && (
+            <span>
+              <span className="text-white">. .</span> .
+            </span>
+          )}
+          {currentSlideIndex % 3 === 2 && (
+            <span>
+              <span className="text-white">. . .</span>
+            </span>
+          )}
         </p>
       </div>
     </div>
