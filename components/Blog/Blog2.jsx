19aaa4f42243d6ac70923abbbde83e74e1743d83
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-10-23 22:03:01 +0530

    faq sorted

--- components/Blog/Blog2.jsx
+++ components/Blog/Blog2.jsx
@@ -10,11 +10,11 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 import Popup from "../common/Popup";
+import FaqImport from "../helper/FaqImport";
 
 
 
 const data = [
-  //Data to be fetched from API and replaced
   {
     title: "Introduction Email Sample: Connect and Impress with Ease",
     content:
@@ -42,39 +42,7 @@ const data = [
   },
 ];
 
-const faqs = [
-  // Data to be fetched from API and replaced
-  {
-    question: "Do I need to pay for it?",
-    answer:
-      "No, You don’t need to pay for it. You can play infinite times without even paying a single penny unless you want to either purchase in-game currencies or get more value by getting Capital Rush’s Pro Subscription.. which would give you an in-depth analysis and feedback on your investing strategies and how you can grow your money even more.",
-  },
-  {
-    question: "Is this a kind of gambling ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    question: "How is Capital Rush different ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    question: "What kind of investment categories do you have ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    question: "I don’t have knowledge about Market, Can I pay it ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    question: "How does it work ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-];
+const faqs = FaqImport;
 
 const Blog2 = () => {
 
@@ -164,6 +132,8 @@ const Blog2 = () => {
         </ul>
       </div>
 
+
+  {/* Large Blog */}
       <div className="">
         <div data-aos="fade-up" data-aos-delay="200"  data-aos-duration="1000" className="flex flex-col mb:flex-row mb:ml-32 ">
           <div className="hidden 2xl:block">
@@ -194,6 +164,9 @@ const Blog2 = () => {
               width={330}
             />
           </div>
+
+
+
           <Link
             href={`/blog/${encodeURIComponent(options[nav - 1])}`}
             className=" no-underline"
