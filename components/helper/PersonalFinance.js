e625c3925c91029bbb1958a1608ed6855dec6cba
Yogesh-Lakhpatani <yogesh@metronometech.com>
2023-12-09 10:40:12 +0530

    09122020 1040

--- components/helper/PersonalFinance.js
+++ components/helper/PersonalFinance.js
@@ -377,78 +377,91 @@ const PersonalFinance = [
       },
     ],
   },
-  // {
-  //   tagid: 1,
-  //   Sno: 4,
-  //   type: "Personal Finance",
-  //   blog_heading: "Post 4",
-  //   blog_subheading:
-  //     "I apologize for the confusion. It seems there might be an issue with the curly braces used temprary data is filled here and need to belffeplaced in future",
-  //   thumbnail:
-  //     "https://res.cloudinary.com/dbum1emnc/image/upload/v1699713356/stockimage_w7lnau.png",
-  //   profile:
-  //     "https://res.cloudinary.com/dbum1emnc/image/upload/v1699713427/dhananjay.jpg_dyhsom.png",
-  //   name: "temporary data4",
-  //   date: "26 JULY, 2023",
-  //   readTime: " 15 MIN READ",
-  //   nameLink: "https://woodpecker.co/blog/author/jj/",
-  //   tableContent: [
-  //     {
-  //       key: "3 key technical settings you need to check",
-  //     },
-  //     {
-  //       key: "Additional things to keep in mind",
-  //     },
+  {
+    tagid: 1,
+    Sno: 4,
+    type: "Personal Finance",
+    blog_heading: "How to Become Your Own Stock Analyst",
+    blog_subheading:
+      "Are you a novice in the world of stock market investments, seeking valuable share market tips? Perhaps you've heard about the potential rewards of venturing into the Indian stock market and wish to take your first steps. You've come to the right place. In this blog, we'll guide you through the process of becoming a proficient stock analyst, equipping you with the knowledge to make prudent investment decisions. To make this journey engaging and interactive, we'll introduce you to Capital Rush—a dynamic investment strategy game that seamlessly blends financial education with captivating gameplay.",
+    thumbnail:
+      "https://res.cloudinary.com/dvjqxitb1/image/upload/v1702097342/Blog19_ztn8ei.jpg",
+    profile:
+      "https://res.cloudinary.com/dvjqxitb1/image/upload/v1700297222/Yogesh_Lakhpatani-_Photo_mjnvfq.jpg",
+    name: "Yogesh Lakhpatani",
+    date: "9 DEC, 2023",
+    readTime: "",
+    nameLink: "",
+    tableContent: [
+      {
+        key: "Stock Analysis: A Methodical Approach",
+      },
+      {
+        key: "Capital Rush: Transforming Learning into Play",
+      },
+      {
+        key: "Conclusion",
+      },
       
-  //   ],
-  //   notice:
-  //     "If . your email doesn’t reach the inbox of the recipient, your campaign will be doomed to fail. No prospect will interact with your content, no matter how great it is. That’s why you need to check three essential technical settings before scheduling the first shipment.",
-  //   data: [
-  //     {
-  //       heading: "3 key technical settings you need to check",
-  //       imageLink:
-  //         "https://res.cloudinary.com/dbum1emnc/image/upload/v1699713356/stockimage_w7lnau.png",
-  //       keypoints: [
-  //         {
-  //           heading: "1. SPF/DKIM configuration",
-  //           details:
-  //             "SPF (Sender Policy Framework) DNS records list servers authorized to deliver emails from a domain. It verifies that your domain’s mail servers and IP addresses send messages.DKIM, on the other hand, verifies a company’s email and helps identify phony email addresses, fights spam, and prevents spoofing and identity theft.You need to make sure that both are correctly configured to avoid any shipment issues.",
-  //         },
-  //         {
-  //           heading: "2. Domain age",
-  //           details:
-  //             "Sending out multiple emails from a relatively young domain is risky, as the messages may look suspicious to providers – and go straight to spam. Check the age of your client’s domain to understand what you’re working with and adjust your course of action.",
-  //         },
-  //         {
-  //           heading: "3. Check if the client is blacklisted",
-  //           details:
-  //             "This one is a must-have! Finally, make sure that your client’s domain isn’t blacklisted anywhere. If you discover that it is, there are ways to get the domain off the blacklist – and accomplishing that will have a huge impact on your client (and it’s also bound to impress your client!).",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       heading: "Additional things to keep in mind",
-  //       imageLink: "",
-  //       keypoints: [
-  //         {
-  //           heading: "1. Set the right “from” name",
-  //           details:
-  //             "Where does the email come from? The right “from” name can make an email seem more real if it shows that it was written by a real person. Adding a suitable picture is also important – the recipient might click on the sender’s profile, and if they see a stock photo, they’ll quickly move this email to spam (which, obviously, will have catastrophic consequences for your entire campaign).",
-  //         },
-  //         {
-  //           heading: "2. Polish your email signature",
-  //           details:
-  //             "The signature needs to be simple and clean. Avoid including too many links. Add a link to the website and a selection of the client’s social media channels. Make sure that the signature gives the impression that the email was sent by a real person.",
-  //         },
-  //         {
-  //           heading: "3. Start small",
-  //           details:
-  //             "At the beginning of your campaign, send just a few messages per day to check if there are any problems or if your emails are landing in the spam folder. If you start getting responses, you’re ready to increase the sending volume.",
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
+    ],
+    notice:
+      "Are you a novice in the world of stock market investments, seeking valuable share market tips? Perhaps you've heard about the potential rewards of venturing into the Indian stock market and wish to take your first steps. You've come to the right place. In this blog, we'll guide you through the process of becoming a proficient stock analyst, equipping you with the knowledge to make prudent investment decisions. To make this journey engaging and interactive, we'll introduce you to Capital Rush—a dynamic investment strategy game that seamlessly blends financial education with captivating gameplay.",
+    data: [
+      {
+        heading: "Stock Analysis: A Methodical Approach",
+        imageLink:
+          "https://res.cloudinary.com/dvjqxitb1/image/upload/v1702097342/Blog19_ztn8ei.jpg",
+        keypoints: [
+          {
+            heading: "",
+            details:
+              "Investing in the stock market is a journey that demands time, patience, and, most importantly, a structured approach. Even seasoned investors rely on stock analysis to navigate the complex world of stocks. So, let's embark on your journey to becoming a proficient stock analyst.",
+          },
+          {
+            heading: "Starting Your Stock Analysis Journey",
+            details:
+              "As a novice investor, it's advisable to commence with what you already know. Begin by analyzing companies whose products or services you are familiar with. Your existing knowledge can be a valuable asset as you delve into the intricate world of stock analysis. Don't rush to diversify your portfolio; instead, focus on a select few stocks initially.",
+          },
+          {
+            heading: "Invest in the Stock Market:",
+            details:
+              "Kickstart your stock market journey by investing in companies you're well-acquainted with.<br/><br/>Elements of Analysis: When conducting stock analysis, it's crucial to take various factors into account:<br/><br/>1. Industry Analysis: Start by gaining insights into the industry in which the company operates. Assess the industry's growth prospects, competitive landscape, and potential challenges. Industries showing promising growth trends often offer the most rewarding investment opportunities.<br/><br/>2. Indian Stock Market: India presents a plethora of investment opportunities across diverse industries.<br/><br/>3. Business Model Evaluation: Dive into the company's business model. Understand how it generates revenue and whether it possesses a competitive edge. A sustainable business model is indicative of long-term success.",
+          },
+          {
+            heading: "Share Market Tips:",
+            details:
+              "While share market tips are valuable, always supplement them with your own analysis to validate their credibility.<br/><br/>1. Financial Health Check: Delve into the company's financial statements. Scrutinize revenue figures, profit margins, and debt levels. A financially robust company is better equipped to weather economic uncertainties.<br/><br/>2. Assessing Management Quality: Evaluate the caliber of the company's management team. Are they seasoned and reliable? Competent leadership is a crucial factor in a company's triumph.<br/><br/>",
+          },
+          {
+            heading: "Beginner's Guide to the Stock Market:",
+            details:
+              "Even as a beginner, you can gauge management quality through research.<br/><br/> 1. Growth Prospects: Consider the company's potential for growth. Is it expanding into new markets? Launching innovative products? Sustainable growth bodes well for your investment.<br/><br/>2. Valuation Analysis: Determine whether the stock is undervalued or overvalued. Compare its current price to its intrinsic value. Stocks trading below their intrinsic value may present promising investment opportunities. Investing in the Stock Market: Profitable investing in the stock market hinges on well-informed decisions rooted in sound valuation analysis.<br/><br/>",
+          },
+        ],
+      },
+      {
+        heading: "Capital Rush: Transforming Learning into Play",
+        imageLink: "",
+        keypoints: [
+          {
+            heading: "",
+            details:
+              "Now that you've grasped the fundamentals of stock analysis, let's introduce you to Capital Rush. Beyond being just another mobile game, Capital Rush is a financial education platform that gamifies the art of stock market investing.<br/>Imagine learning about stock analysis, portfolio management, and risk mitigation through an engaging game. Capital Rush offers precisely that experience.",
+          },
+          {
+            heading: "Single Player Mode",
+            details:
+              "In the single-player mode, you can construct and nurture your virtual investment portfolio. Test your analytical skills, make investment decisions, and gain experience without risking actual capital.",
+          },
+          {
+            heading: "Multiplayer Mode",
+            details:
+              "At the beginning of your campaign, send just a few messages per day to check if there are any problems or if your emails are landing in the spam folder. If you start getting responses, you’re ready to increase the sending volume.",
+          },
+        ],
+      },
+    ],
+  },
   // {
   //   tagid: 1,
   //   Sno: 5,
