b9b742f3fdb5f5884b5788ce6fcb6d71643e4097
Yogesh-Lakhpatani <yogesh@metronometech.com>
2023-12-09 11:08:11 +0530

    09122020 1108

--- components/helper/InvestingEss.js
+++ components/helper/InvestingEss.js
@@ -191,78 +191,105 @@ const investingEss = [
       },
     ],
   },
-  // {
-  //   tagid: 2,
-  //   Sno: 3,
-  //   type: "Investing Essentials/Guide",
-  //   blog_heading: " Investing Essentials blog content is here: Post 3",
-  //   blog_subheading:
-  //     "I apologize for the confusion. It seems there might be an issue with the curly braces used temprary data is filled here and need to belffeplaced in future",
-  //   thumbnail:
-  //     "https://res.cloudinary.com/dbum1emnc/image/upload/v1699713356/stockimage_w7lnau.png",
-  //   profile:
-  //     "https://res.cloudinary.com/dbum1emnc/image/upload/v1699713427/dhananjay.jpg_dyhsom.png",
-  //   name: "temporary data2",
-  //   date: "16 JUNE, 2023",
-  //   readTime: " 15 MIN READ",
-  //   nameLink: "https://woodpecker.co/blog/author/jj/",
-  //   tableContent: [
-  //     {
-  //       key: "3 key technical settings you need to check",
-  //     },
-  //     {
-  //       key: "Additional things to keep in mind",
-  //     },
+  {
+    tagid: 2,
+    Sno: 3,
+    type: "Investing Essentials/Guide",
+    blog_heading: "Value vs. Growth Investing: Choosing the Right Strategy for You",
+    blog_subheading:
+      "In the expansive realm of the equity market, two prominent investment strategies take center stage: value investing and growth investing. These strategies epitomize distinct approaches to constructing a portfolio and can significantly influence your investment outcomes. If you're seeking information about the share market and contemplating an endeavor to invest in the market, understanding the disparities between these two strategies is pivotal. It enables you to make well-informed decisions that are in sync with your financial objectives and risk tolerance.",
+    thumbnail:
+      "https://res.cloudinary.com/dvjqxitb1/image/upload/v1702099434/Blog8_vc8ekh.jpg",
+    profile:
+      "https://res.cloudinary.com/dvjqxitb1/image/upload/v1700297222/Yogesh_Lakhpatani-_Photo_mjnvfq.jpg",
+    name: "Yogesh Lakhpatani",
+    date: "9 DEC, 2023",
+    readTime: "",
+    nameLink: "",
+    tableContent: [
+      {
+        key: "Value Vs Growth",
+      },
+      {
+        key: "Aligning Strategy with Your Goals and Tolerance",
+      },
+      {
+        key: "Educational Gaming with Capital Rush: Where Learning Meets Fun",
+      },
+      {
+        key: "Conclusion",
+      },
       
-  //   ],
-  //   notice:
-  //     "If . your email doesn’t reach the inbox of the recipient, your campaign will be doomed to fail. No prospect will interact with your content, no matter how great it is. That’s why you need to check three essential technical settings before scheduling the first shipment.",
-  //   data: [
-  //     {
-  //       heading: "3 key technical settings youneed to check",
-  //       imageLink:
-  //         "https://res.cloudinary.com/dbum1emnc/image/upload/v1699713356/stockimage_w7lnau.png",
-  //       keypoints: [
-  //         {
-  //           heading: "1. SPF/DKIM configuration",
-  //           details:
-  //             "SPF (Sender Policy Framework) DNS records list servers authorized to deliver emails from a domain. It verifies that your domain’s mail servers and IP addresses send messages.DKIM, on the other hand, verifies a company’s email and helps identify phony email addresses, fights spam, and prevents spoofing and identity theft.You need to make sure that both are correctly configured to avoid any shipment issues.",
-  //         },
-  //         {
-  //           heading: "2. Domain age",
-  //           details:
-  //             "Sending out multiple emails from a relatively young domain is risky, as the messages may look suspicious to providers – and go straight to spam. Check the age of your client’s domain to understand what you’re working with and adjust your course of action.",
-  //         },
-  //         {
-  //           heading: "3. Check if the client is blacklisted",
-  //           details:
-  //             "This one is a must-have! Finally, make sure that your client’s domain isn’t blacklisted anywhere. If you discover that it is, there are ways to get the domain off the blacklist – and accomplishing that will have a huge impact on your client (and it’s also bound to impress your client!).",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       heading: "Additional things to keep in mind",
-  //       imageLink: "",
-  //       keypoints: [
-  //         {
-  //           heading: "1. Set the right “from” name",
-  //           details:
-  //             "Where does the email come from? The right “from” name can make an email seem more real if it shows that it was written by a real person. Adding a suitable picture is also important – the recipient might click on the sender’s profile, and if they see a stock photo, they’ll quickly move this email to spam (which, obviously, will have catastrophic consequences for your entire campaign).",
-  //         },
-  //         {
-  //           heading: "2. Polish your email signature",
-  //           details:
-  //             "The signature needs to be simple and clean. Avoid including too many links. Add a link to the website and a selection of the client’s social media channels. Make sure that the signature gives the impression that the email was sent by a real person.",
-  //         },
-  //         {
-  //           heading: "3. Start small",
-  //           details:
-  //             "At the beginning of your campaign, send just a few messages per day to check if there are any problems or if your emails are landing in the spam folder. If you start getting responses, you’re ready to increase the sending volume.",
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
+    ],
+    notice:
+      "In the expansive realm of the equity market, two prominent investment strategies take center stage: value investing and growth investing. These strategies epitomize distinct approaches to constructing a portfolio and can significantly influence your investment outcomes. If you're seeking information about the share market and contemplating an endeavor to invest in the market, understanding the disparities between these two strategies is pivotal. It enables you to make well-informed decisions that are in sync with your financial objectives and risk tolerance.",
+    data: [
+      {
+        heading: "Value Vs Growth Aligning the strategy",
+        imageLink:
+          "https://res.cloudinary.com/dvjqxitb1/image/upload/v1702099434/Blog8_vc8ekh.jpg",
+        keypoints: [
+          {
+            heading: "1. Value Investing: Uncovering Hidden Gems",
+            details:
+              "Value investing centers on the principle of identifying stocks that are trading below their intrinsic value. In essence, value investors believe that certain stocks are presently undervalued by the market, offering an opportunity to acquire them at a discount. This strategy entails meticulous analysis of a company's financials, delving into factors such as earnings, dividends, and book value. The aim is to pinpoint companies with robust fundamentals that the market has temporarily underestimated.<br/><br/>Investors inclined toward value often gravitate towards companies grappling with transient setbacks or industries currently out of favor. By investing in these undervalued stocks, they aspire to capitalize on the potential for future growth and a subsequent uptick in stock price. Value investing demands patience and a contrarian mindset, given that value stocks may take time to regain market favor.",
+          },
+          {
+            heading: "2. Growth Investing: Navigating the Path to Prosperity",
+            details:
+              "Conversely, growth investing revolves around companies exhibiting substantial growth potential. Instead of fixating solely on current valuation, growth investors prioritize companies projected to experience above-average expansion in terms of revenue and earnings. Often, these companies are at the forefront of innovation, operating in sectors ripe with significant growth prospects.<br/><br/>Growth investors are inclined to pay a premium for stocks that display promise, banking on the anticipation that the company's earnings will surge over time. This strategy resonates particularly with those comfortable embracing heightened risk and pursuing the potential for substantial returns. It's important to note that growth stocks can be more volatile, witnessing more significant price fluctuations compared to their value counterparts.",
+          },
+          {
+            heading: "3. Aligning Strategy with Your Goals and Tolerance",
+            details:
+              "The choice between value and growth investing hinges on your individual investment goals, risk tolerance, and investment horizon. Let's delve into how each strategy aligns with different types of investors: <br/><br/>1. Conservative Investors: If you lean towards a more cautious approach and prioritize capital preservation, value investing might be your forte. This strategy seeks to mitigate the risk of overpaying for stocks, while spotlighting the company's financial robustness. <br/><br/>Aggressive Investors: For those at ease with heightened risk and aiming for accelerated growth, growth investing could be an alluring path. While it entails amplified volatility, the prospect of significant returns can be enticing for those with a more extended investment horizon.<br/><br/>2. Balanced Investors: Certain investors opt for a balanced approach, melding elements of both value and growth investing. This involves diversifying your portfolio to encompass both undervalued stocks with growth potential and stocks of companies showcasing consistent expansion.<br/><br/>",
+          },
+        ],
+      },
+      {
+        heading: "Educational Gaming with Capital Rush: Where Learning Meets Fun",
+        imageLink: "",
+        keypoints: [
+          {
+            heading: "",
+            details:
+              "By this juncture, you may be contemplating how to pragmatically employ these strategies and garner hands-on experience sans the peril of actual financial loss. Enter Capital Rush. As a trading and investment-based simulation game, Capital Rush offers a novel avenue to grasp the essence of effective investing in the equity market. Similar to how Monopoly breathes life into real estate dealings, Capital Rush fabricates a lifelike milieu where users can learn and enact real investment decisions.<br/><br/>Capital Rush immerses you in a dynamic world where you can experiment with both value and growth investing strategies devoid of real-world risk. Whether you aspire to unearth hidden gems through value investing or navigate the path to prosperity via growth investing, Capital Rush furnishes an interactive platform to hone your skills and glean insights into the intricacies of the market.",
+          },
+          // {
+          //   heading: "2. Polish your email signature",
+          //   details:
+          //     "The signature needs to be simple and clean. Avoid including too many links. Add a link to the website and a selection of the client’s social media channels. Make sure that the signature gives the impression that the email was sent by a real person.",
+          // },
+          // {
+          //   heading: "3. Start small",
+          //   details:
+          //     "At the beginning of your campaign, send just a few messages per day to check if there are any problems or if your emails are landing in the spam folder. If you start getting responses, you’re ready to increase the sending volume.",
+          // },
+        ],
+      },
+      {
+        heading: "Conclusion",
+        imageLink: "",
+        keypoints: [
+          {
+            heading: "",
+            details:
+              "As you embark on your investment voyage, bear in mind that the decision between value and growth investing should seamlessly mesh with your personal financial aspirations. Each strategy boasts its merits and contemplations, and there exists no definitive right or wrong choice. Your appetite for risk, investment horizon, and objectives will invariably steer your selection.<br/><br/>In your quest for enlightenment about the share market and your yearning to invest in the market, embrace the educational potency of Capital Rush. It transcends mere gaming—it's a transformative educational odyssey that empowers you to take informed investment strides. So, why delay? Embark on your journey with Capital Rush today and unlock the potential to metamorphose into a more astute and strategic investor.<br/><br/>Ready to embark on your investment adventure? Download Capital Rush and set sail on your journey today!",
+          },
+          // {
+          //   heading: "2. Polish your email signature",
+          //   details:
+          //     "The signature needs to be simple and clean. Avoid including too many links. Add a link to the website and a selection of the client’s social media channels. Make sure that the signature gives the impression that the email was sent by a real person.",
+          // },
+          // {
+          //   heading: "3. Start small",
+          //   details:
+          //     "At the beginning of your campaign, send just a few messages per day to check if there are any problems or if your emails are landing in the spam folder. If you start getting responses, you’re ready to increase the sending volume.",
+          // },
+        ],
+      },
+    ],
+  },
   // {
   //   tagid: 2,
   //   Sno: 4,
