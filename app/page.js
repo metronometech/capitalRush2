b30eb966883c7e94b58195eeaa25eca941581cc8
Nilesh Sharma <nilesh.12th@gmail.com>
2024-11-03 18:46:05 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,41 +4,24 @@ import Home1 from "@/components/home/Home1";
 import Home2 from "@/components/home/Home2";
 import Home3 from "@/components/home/Home3";
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/router';
 
 export default function Home() {
   const [link, updateLink] = useState("/");
+  const [showOverlay, setShowOverlay] = useState(false);
 
   function setAppStoreLink() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    // Check for iOS (iPhone, iPad, iPod)
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
       updateLink("https://apps.apple.com/in/app/capital-rush/id1615705403");
-    } 
-    // Check for Android
-    else if (/android/i.test(userAgent)) {
+    } else if (/android/i.test(userAgent)) {
       updateLink("https://play.google.com/store/apps/details?id=com.metronome.capital");
     }
   }
 
   function openUnityDeepLink(contestId) {
     const unityLink = `CaptialRush://ContestShare?${contestId}`;
-    const fallbackLink = "https://play.google.com/store/apps/details?id=com.metronome.capital";
-  
-    // Try to open the deep link
-    const startTime = Date.now();
-    window.open(unityLink, '_blank');
-  
-    // Check after a short delay if the app opened or not
-    setTimeout(() => {
-      const elapsedTime = Date.now() - startTime;
-      // If the user was not redirected (i.e., the app is not installed),
-      // the elapsed time will be short, and we redirect to Play Store
-      if (elapsedTime < 2000) { // 2 seconds is usually enough to detect redirection
-        window.open(fallbackLink, '_blank');
-      }
-    }, 1500); // Delay to check if app opened (1.5 seconds)
+    window.location.href = unityLink;
+    setShowOverlay(false); // Remove overlay after clicking
   }
 
   useEffect(() => {
@@ -47,15 +30,30 @@ export default function Home() {
     // Check for "contestId" in the URL query parameters
     const params = new URLSearchParams(window.location.search);
     const contestId = params.get("contestId");
-    
+
     if (contestId) {
-      // Run the Unity deep link function if "contestId" is found
-      openUnityDeepLink(contestId);
+      setShowOverlay(true); // Show overlay if "contestId" is present
     }
   }, []);
 
   return (
     <div className="flex flex-col bg-[#100B26] w-full text-31xl font-poppins">
+      {/* Overlay */}
+      {showOverlay && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+          <button
+            onClick={() => openUnityDeepLink(new URLSearchParams(window.location.search).get("contestId"))}
+            className="bg-white text-black font-semibold py-2 px-4 rounded-lg text-xl"
+          >
+            Check out the contest
+          </button>
+          <style jsx global>{`
+            body {
+              overflow: hidden;
+            }
+          `}</style>
+        </div>
+      )}
       <Home1 />
       <Home2 link={link} />
       <Home3 link={link} />
