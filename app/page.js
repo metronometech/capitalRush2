c43a194ab28007c48dc00c0cbf41a136e36940ac
Nilesh Sharma <nilesh.12th@gmail.com>
2024-11-03 18:16:23 +0530

    added the redirection

--- app/page.js
+++ app/page.js
@@ -1,51 +1,65 @@
-"use client"
+"use client";
 import FooterElem from "@/components/common/FooterElem";
 import Home1 from "@/components/home/Home1";
 import Home2 from "@/components/home/Home2";
 import Home3 from "@/components/home/Home3";
 import { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
 
 export default function Home() {
-
-  
-  const [link,updatelink]=useState("/");
+  const [link, updateLink] = useState("/");
 
   function setAppStoreLink() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    
+
     // Check for iOS (iPhone, iPad, iPod)
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        // document.getElementById('appStoreLink').href = 'https://apps.apple.com/us/app/your-app-name/id1234567890'; // Replace with your actual App Store link
-        updatelink("https://apps.apple.com/in/app/capital-rush/id1615705403")
+      updateLink("https://apps.apple.com/in/app/capital-rush/id1615705403");
     } 
     // Check for Android
     else if (/android/i.test(userAgent)) {
-        // document.getElementById('playStoreLink').href = 'https://play.google.com/store/apps/details?id=com.yourapp.package'; // Replace with your actual Play Store link
-        updatelink("https://play.google.com/store/apps/details?id=com.metronome.capital")
+      updateLink("https://play.google.com/store/apps/details?id=com.metronome.capital");
     }
-}
+  }
 
-useEffect(() => {
-  setAppStoreLink();
-  // const tempTime = Date.now() + (1*60*60*1000);
-}, [])
-
+  function openUnityDeepLink(contestId) {
+    const unityLink = `CaptialRush://ContestShare?${contestId}`;
+    const fallbackLink = "https://play.google.com/store/apps/details?id=com.metronome.capital";
   
+    // Try to open the deep link
+    const startTime = Date.now();
+    window.open(unityLink, '_blank');
+  
+    // Check after a short delay if the app opened or not
+    setTimeout(() => {
+      const elapsedTime = Date.now() - startTime;
+      // If the user was not redirected (i.e., the app is not installed),
+      // the elapsed time will be short, and we redirect to Play Store
+      if (elapsedTime < 2000) { // 2 seconds is usually enough to detect redirection
+        window.open(fallbackLink, '_blank');
+      }
+    }, 1500); // Delay to check if app opened (1.5 seconds)
+  }
+
+  useEffect(() => {
+    setAppStoreLink();
+
+    // Check for "contestId" in the URL query parameters
+    const params = new URLSearchParams(window.location.search);
+    const contestId = params.get("contestId");
+    
+    if (contestId) {
+      // Run the Unity deep link function if "contestId" is found
+      openUnityDeepLink(contestId);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col bg-[#100B26] w-full text-31xl font-poppins">
-    <Home1 />
-    <Home2 link={link} />
-    <Home3 link={link} />
-    <FooterElem />
+      <Home1 />
+      <Home2 link={link} />
+      <Home3 link={link} />
+      <FooterElem />
     </div>
   );
-}
-
-{
-  /* <img
-  className=" h-[95.54%] w-[95.49%] top-[4.09%] right-[2.64%] bottom-[0.37%] left-[1.88%] max-w-full  max-h-full"
-  alt=""
-  src="/tiles-path.svg"
-/> */
 }
