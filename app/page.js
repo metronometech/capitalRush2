116258b49a7eb280f1ef274dcdcd0583504cb335
Nilesh Sharma <nilesh.12th@gmail.com>
2024-11-06 18:33:10 +0530

    redirection to stores

--- app/page.js
+++ app/page.js
@@ -9,51 +9,55 @@ import { useRouter } from 'next/router';
 export default function Home() {
   const [link, updateLink] = useState("/");
 
-  // function setAppStoreLink() {
-  //   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  function setAppStoreLink() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  //   // Check for iOS (iPhone, iPad, iPod)
-  //   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-  //     updateLink("https://apps.apple.com/in/app/capital-rush/id1615705403");
-  //   } 
-  //   // Check for Android
-  //   else if (/android/i.test(userAgent)) {
-  //     updateLink("https://play.google.com/store/apps/details?id=com.metronome.capital");
-  //   }
-  // }
+    // Check for iOS (iPhone, iPad, iPod)
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      updateLink("https://apps.apple.com/in/app/capital-rush/id1615705403");
+    } 
+    // Check for Android
+    else if (/android/i.test(userAgent)) {
+      updateLink("https://play.google.com/store/apps/details?id=com.metronome.capital");
+    }
+  }
 
-  // function openUnityDeepLink(contestId) {
-  //   const unityLink = `http://capitalrush.io/?${contestId}`;
-  //   const fallbackLink = "https://play.google.com/store/apps/details?id=com.metronome.capital";
+  function openLink(contestId) {
+    let fallbackLink;
+  
+    // Detect iOS devices
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     
-  //   // Record start time
-  //   const startTime = Date.now();
-  
-  //   // Try opening the deep link directly
-  //   window.location.href = unityLink;
-  
-  //   // Fallback if the app does not open
-  //   setTimeout(() => {
-  //     const elapsedTime = Date.now() - startTime;
-  
-  //     if (elapsedTime < 3000) { // Assume app didn't open within 3 seconds
-  //       window.location.href = fallbackLink;
-  //     }
-  //   }, 1500); // 1.5 second delay to allow the app to open
-  // }
-
-  // useEffect(() => {
-  //   setAppStoreLink();
-
-  //   // Check for "contestId" in the URL query parameters
-  //   const params = new URLSearchParams(window.location.search);
-  //   const contestId = params.get("contestId");
+    // Detect Android devices
+    const isAndroid = /Android/.test(navigator.userAgent);
     
-  //   if (contestId) {
-  //     // Run the Unity deep link function if "contestId" is found
-  //     openUnityDeepLink(contestId);
-  //   }
-  // }, []);
+    // Detect Windows devices
+    const isWindows = /Windows/.test(navigator.userAgent);
+  
+    if (isIOS) {
+      fallbackLink = "https://apps.apple.com/in/app/capital-rush/id1615705403";
+    } else if (isAndroid || isWindows) {
+      fallbackLink = "https://play.google.com/store/apps/details?id=com.metronome.capital";
+    } else {
+      // Default link in case the device is not recognized
+      fallbackLink = "https://www.metronome.com/capital-rush";
+    }
+  
+    window.location.href = fallbackLink;
+  }
+
+  useEffect(() => {
+    setAppStoreLink();
+
+    // Check for "contestId" in the URL query parameters
+    const params = new URLSearchParams(window.location.search);
+    const contestId = params.get("q");
+    
+    if (contestId) {
+      // Run the Unity deep link function if "contestId" is found
+      openLink(contestId);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col bg-[#100B26] w-full text-31xl font-poppins">
